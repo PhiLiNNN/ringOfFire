d21b6e35dbcbc3581de0409d74c5020311e51b2d
PhiLiNNN <philipp_wendschuch@web.de>
2024-05-31 18:14:00 +0200

    added cards animation, created game obj incluiding pushing all  cards  in   the stack and shuffle it afterwards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
+  pickCardAnimation = false;
+  game: Game = new Game();
+  currentCard: string = '';
 
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() || '';
+      this.pickCardAnimation = true;
+    }
+
+    setTimeout(() => {
+      this.pickCardAnimation = false;
+    }, 1500);
+  }
 }
---
+++ src/app/models/game.ts
@@ -0,0 +1,31 @@
+export class Game {
+  public players: string[] = [];
+  public stack: string[] = [];
+  public playedCard: string[] = [];
+  public currentPlayer: number = 0;
+  constructor() {
+    for (let index = 1; index < 14; index++) {
+      this.stack.push('ace_' + index);
+      this.stack.push('clubs_' + index);
+      this.stack.push('diamonds_' + index);
+      this.stack.push('hearts_' + index);
+    }
+    shuffle(this.stack);
+  }
+}
+
+function shuffle(arr: string[]) {
+  let currentIndex = arr.length;
+  let temporaryValue: string;
+  let randomIndex: number;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = arr[currentIndex];
+    arr[currentIndex] = arr[randomIndex];
+    arr[randomIndex] = temporaryValue;
+  }
+  return arr;
+}
